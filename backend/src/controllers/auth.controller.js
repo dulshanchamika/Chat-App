2c3096b8e991fb5337d496baf5c3a8f1b2504ffd
dulshanchamika <dulshanchamika2@gmail.com>
2025-10-30 01:48:17 +0530

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -51,12 +53,18 @@ export const signup = async (req, res) => {
         profilePic: newUser.prifilePic,
       });
 
+      try {
+        await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+      } catch (error) {
+        console.error("Failed to send welcome email:", error);
+      }
+
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
 
   } catch (error) {
     console.log("Error in signup controller:", error);
-    res.status(500).json({ message: "Internal server error" })
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = process.env;
+  const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT SECRET is not configured");
   }
@@ -14,7 +15,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, //prevent XSS attacks: cors-site scripting
     sameSite: "strict", //CSRF attacks
-    secure: process.env.NODE_ENV === "developmnet" ? false : true, //http - false //https - true
+    secure: ENV.NODE_ENV === "developmnet" ? false : true, //http - false //https - true
   });
 
   return token;
