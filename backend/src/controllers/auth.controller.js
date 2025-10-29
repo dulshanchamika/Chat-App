584b5835ee272916eff4d9662d5893fa7281817b
dulshanchamika <dulshanchamika2@gmail.com>
2025-10-30 00:32:22 +0530

    code optimizations

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -35,8 +35,14 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res)
-      await newUser.save()
+
+      //in here the user is saved at the data base after generating the jwt token
+      // generateToken(newUser._id, res)
+      // await newUser.save()
+
+      //here user saves before creating the token
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
