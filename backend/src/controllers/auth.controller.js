719976999268562fbc1babab879dd6f22a707f8a
dulshanchamika <dulshanchamika2@gmail.com>
2025-10-30 17:08:55 +0530

    setting up the rate limitter

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,7 +4,6 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import cloudinary from "../lib/cloudinary.js";
-import router from "../routes/auth.route.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body
