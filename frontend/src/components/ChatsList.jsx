c5f3f1f2d10e0691057e96c8f04677141ba66863
dulshanchamika <dulshanchamika2@gmail.com>
2025-10-31 19:56:22 +0530

    finalized

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "../store/useAuthStore";
 
 function ChatsList() {
   const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
-  // const { onlineUsers } = useAuthStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getMyChatPartners();
@@ -24,8 +24,7 @@ function ChatsList() {
           onClick={() => setSelectedUser(chat)}
         >
           <div className="flex items-center gap-3">
-            {/* ${onlineUsers.includes(chat._id) ? "online" : "offline"} */}
-            <div className={`avatar online`}> 
+            <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}>
               <div className="size-12 rounded-full">
                 <img src={chat.profilePic || "/avatar.png"} alt={chat.fullName} />
               </div>
@@ -37,5 +36,4 @@ function ChatsList() {
     </>
   );
 }
-
 export default ChatsList;
